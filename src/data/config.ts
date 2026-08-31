e7fc5e42c471e4d0b058fe8a12de987036d3df6a
Everth Paredes <epe0413@gmail.com>
2026-08-31 10:59:53 -0600

    feat(footer): integrate official social media links and SVG icons

---
+++ src/data/config.ts
@@ -0,0 +1,8 @@
+// src/data/config.ts
+
+export const CONFIG_NEGOCIO = {
+    whatsappNumero: "50587291404",
+    facebookUrl: "https://www.facebook.com/profile.php?id=61576296247591",
+    instagramUrl: "https://www.instagram.com/bienestarconeverth/",
+    mensajeBase: "Hola, vi la información en MyOmniGuide y me interesa asesoría sobre"
+};
